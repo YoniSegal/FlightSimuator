6b035ed147d55edd796fe30475d9053a13b7d640
BATEL <batel.cohen100@gmail.com>
2019-04-24 14:25:54 +0300

    autoPilot Binding

---
+++ FlightSimuator/Model/AutoPilotModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightSimuator.Model
+{
+    class AutoPilotModel
+    {
+        // send commands to the simulator
+        public void SendCommands(string input)
+        {
+            /*
+            if (Commands.Instance.Connected)
+            {
+                new Task(delegate ()
+                {
+                    Commands.Instance.SendCommands(input);
+                }).Start();
+            }C:\Users\BATEL\Source\Repos\YoniSegal\FlightSimuator\FlightSimuator\ViewModels\SettingHandler.cs
+            */
+
+        }
+    }
+}
