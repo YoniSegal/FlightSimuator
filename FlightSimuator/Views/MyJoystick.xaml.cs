6426bf7efe9ac2c25194f48ad27150f7c6ae8112
Yoni Segal <yonisegal13@gmail.com>
2019-04-28 21:14:11 +0300

    Deleting Comments

--- FlightSimuator/Views/MyJoystick.xaml.cs
+++ FlightSimuator/Views/MyJoystick.xaml.cs
@@ -1,18 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using FlightSimulator.ViewModels;
 
@@ -23,11 +9,7 @@ namespace FlightSimulator.Views
     /// Interaction logic for MyJoystick.xaml
     /// </summary>
     public partial class MyJoystick : INotifyPropertyChanged
-    {
-  
-        
-       private JoystickViewModel joystickVM;
-        
+    {        
         public MyJoystick()
         {
             InitializeComponent();
@@ -38,7 +20,6 @@ namespace FlightSimulator.Views
 
         private void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
-            Console.WriteLine("Something changed!");
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
--- FlightSimuator/Views/Windows/Settings.xaml.cs
+++ FlightSimuator/Views/Windows/Settings.xaml.cs
@@ -1,18 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 using FlightSimulator.Model;
-using FlightSimulator.ViewModels;
 using FlightSimulator.ViewModels.Windows;
 
 namespace FlightSimulator.Views
@@ -39,7 +26,7 @@ namespace FlightSimulator.Views
         private void OkCommand(object sender, RoutedEventArgs e)
         {
             sh.SaveSettings();
-             this.Close();
+            this.Close();
         }
     }
 }
